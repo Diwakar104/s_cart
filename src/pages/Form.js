342343c43a8b0d2b26fed6e5808579ecceeb84a1
Diwakar <diwakar13395@gmail.com>
2023-06-16 00:52:17 +0530

    done validation

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -2,18 +2,33 @@ import React, { useState } from "react";
 import { Box } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { addItem } from "../redux/action";
+import Snackbar from "@mui/material/Snackbar";
+
 function Form() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addItem({ name, price }));
-    setName("");
-    setPrice("");
+    if (name.trim() === "" || price.trim() === "") {
+      setOpen(true);
+      setErrorMessage("Please enter both item name and price.");
+      return;
+    } else {
+      dispatch(addItem({ name, price }));
+      setName("");
+      setPrice("");
+    }
+    // dispatch(addItem({ name, price }));
+    // setName("");
+    // setPrice("");
   };
-
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Box>
       <form onSubmit={handleSubmit}>
@@ -35,6 +50,14 @@ function Form() {
           Add Item
         </button>
       </form>
+      <Snackbar
+        severity={"error"}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={errorMessage}
+        // action={action}
+      />
     </Box>
   );
 }
